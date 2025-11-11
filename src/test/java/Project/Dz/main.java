3c22efbc58cb1155d907c150ecf40d9c2d616402
Dthota <dillibabu.thota@gmail.com>
2025-11-11 11:08:08 +0530

    second commit

---
+++ src/test/java/Project/Dz/main.java
@@ -0,0 +1,17 @@
+package Project.Dz;
+
+import org.testng.annotations.Test;
+
+public class main {
+	
+	@Test
+	public void firstTest() {
+		System.out.println("First Test from main.java in Dz Project");
+	}
+	
+	@Test
+	public void secondTest() {
+		System.out.println("Second Test from main.java in Dz Project");
+	}
+
+}
